7c77c703c18f038e459f2cd2460b59670ddbd976
Thalys Augusto <augusto.thalys@gmail.com>
2023-10-31 17:38:47 -0300

    ASDASD

--- sql.sql
+++ sql.sql
@@ -1,3 +1,6 @@
 INSERT INTO membros
 (nome, email, cpf, telefone, endereco, foto, data_cad, data_nasc, igreja, obs, data_batismo, cargo, ativo, estado_civil, membresia, rg, nacionalidade, naturalidade, nome_pai, nome_mae)
-VALUES('Pablo Silva', 'pablos@hotmail.com', '484.154.545-55', '(21) 15154-5454', 'Rua Almeida Campos 150 Bairro Serra Verde - Belo Horizonte - MG', '28-06-2021-14-56-14-01.jpg', '2021-06-28', '1995-06-01', 1, NULL, '0000-00-00', '3', 'Sim', '', 'NULL', NULL, NULL, NULL, NULL, NULL);
+VALUES('Pablo Silva', 'pablos@hotmail.com', '484.154.545-55', '(21) 15154-5454', 'Rua Almeida Campos 150 Bairro Serra Verde - Belo Horizonte - MG', NULL, '2023-10-31', '1995-06-01', 1, NULL, '0000-00-00', '3', 'Sim', '', 'NULL', NULL, NULL, NULL, NULL, NULL);
+
+
+
