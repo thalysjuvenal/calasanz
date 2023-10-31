751ac199fe45acb6993e313e6c91573a4dd8b52a
Thalys Augusto <augusto.thalys@gmail.com>
2023-10-31 18:03:22 -0300

    subindo ajuste

--- sql.sql
+++ sql.sql
@@ -1,6 +1,10 @@
-INSERT INTO membros
-(nome, email, cpf, telefone, endereco, foto, data_cad, data_nasc, igreja, obs, data_batismo, cargo, ativo, estado_civil, membresia, rg, nacionalidade, naturalidade, nome_pai, nome_mae)
-VALUES('Pablo Silva', 'pablos@hotmail.com', '484.154.545-55', '(21) 15154-5454', 'Rua Almeida Campos 150 Bairro Serra Verde - Belo Horizonte - MG', NULL, '2023-10-31', '1995-06-01', 1, NULL, '0000-00-00', '3', 'Sim', '', 'NULL', NULL, NULL, NULL, NULL, NULL);
-
-
-
+SELECT
+    *
+FROM
+    membros
+where
+    igreja = '1'
+    and month(data_nasc) = '10'
+    and day(data_nasc) = '31'
+order by
+    data_nasc asc
