5bf0ad055ea761bc2eb366de9940f568d4783d57
Thalys Augusto <augusto.thalys@gmail.com>
2023-11-01 18:30:21 -0300

    voltando acoes

--- sistema/painel-igreja/notas_moedas/inserir.php
+++ sistema/painel-igreja/notas_moedas/inserir.php
@@ -3,7 +3,7 @@ require_once("../../conexao.php");
 @session_start();
 $id_usuario = @$_SESSION['id_usuario'];
 
-$pagina = 'ofertas';
+$pagina = 'notas_moedas';
 
 $valor = $_POST['valor'];
 $membro = $_POST['membro'];
@@ -14,14 +14,14 @@ $id = @$_POST['id'];
 
 $query_con = $pdo->query("SELECT * FROM membros where id = '$membro'");
 $res_con = $query_con->fetchAll(PDO::FETCH_ASSOC);
-if(count($res_con) > 0){
+if (count($res_con) > 0) {
 	$nome_membro = $res_con[0]['nome'];
-}else{
+} else {
 	$nome_membro = 'Membro Não Informado';
 }
 
 
-if($id == "" || $id == 0){
+if ($id == "" || $id == 0) {
 	$query = $pdo->prepare("INSERT INTO $pagina SET membro = '$membro', valor = :valor, data = '$data', usuario = '$id_usuario', igreja = '$igreja'");
 
 	$query->bindValue(":valor", "$valor");
@@ -29,18 +29,18 @@ if($id == "" || $id == 0){
 	$ult_id = $pdo->lastInsertId();
 
 	//INSIRO NAS MOVIMENTACOES
-$pdo->query("INSERT INTO movimentacoes SET tipo = 'Entrada', movimento = 'Oferta', descricao = '$nome_membro', valor = '$valor', data = '$data', usuario = '$id_usuario', id_mov = '$ult_id', igreja = '$igreja'");
+	$pdo->query("INSERT INTO movimentacoes SET tipo = 'Entrada', movimento = 'Oferta', descricao = '$nome_membro', valor = '$valor', data = '$data', usuario = '$id_usuario', id_mov = '$ult_id', igreja = '$igreja'");
 
-}else{
+} else {
 	require_once("../verificar-tesoureiro.php");
 	$query = $pdo->prepare("UPDATE $pagina SET membro = '$membro', valor = :valor, data = '$data', usuario = '$id_usuario', igreja = '$igreja' where id = '$id'");
 
 	//INSIRO NAS MOVIMENTACOES
-$pdo->query("UPDATE movimentacoes SET descricao = '$nome_membro', valor = '$valor', data = '$data', usuario = '$id_usuario' where id_mov = '$id' and movimento = 'Oferta'");
+	$pdo->query("UPDATE movimentacoes SET descricao = '$nome_membro', valor = '$valor', data = '$data', usuario = '$id_usuario' where id_mov = '$id' and movimento = 'Oferta'");
 
 
-$query->bindValue(":valor", "$valor");
-$query->execute();
+	$query->bindValue(":valor", "$valor");
+	$query->execute();
 
 }
 
