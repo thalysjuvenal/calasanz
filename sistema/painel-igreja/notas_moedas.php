f573c23d4c5d9fcc61d886ee5e43a00ace6f380a
Thalys Augusto <augusto.thalys@gmail.com>
2023-11-01 19:06:23 -0300

    tasda

--- sistema/painel-igreja/notas_moedas.php
+++ sistema/painel-igreja/notas_moedas.php
@@ -138,7 +138,7 @@ require_once("deslogar-secretario.php");
 						<div class="col-md-4">
 							<div class="mb-3">
 								<label for="exampleFormControlInput1" class="form-label">Cédula/Moeda</label>
-								<select name="valor_unitario" class="form-select" required>
+								<select name="valor_unitario" class="form-select" id= "valor_unitario" required>
 									<option value="0.05">R$ 0.05</option>
 									<option value="0.10">R$ 0.10</option>
 									<option value="0.25">R$ 0.25</option>
@@ -157,7 +157,7 @@ require_once("deslogar-secretario.php");
 						<div class="col-md-4">
 							<div class="mb-3">
 								<label for="exampleFormControlInput1" class="form-label">Dízimo/Oferta</label>
-								<select name="dizimo_oferta" class="form-select" required>
+								<select name="dizimo_oferta" class="form-select" id="dizimo_oferta" required>
 									<option value="Dízimo">Dízimo</option>
 									<option value="Oferta">Oferta</option>
 								</select>
