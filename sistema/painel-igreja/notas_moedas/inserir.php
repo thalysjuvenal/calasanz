04da4abf51715be07a196f36b9d6dc2dafcb877a
Thalys Augusto <augusto.thalys@gmail.com>
2023-11-01 21:18:42 -0300

    asdasd

--- sistema/painel-igreja/notas_moedas/inserir.php
+++ sistema/painel-igreja/notas_moedas/inserir.php
@@ -1,6 +1,6 @@
 <?php
 require_once("../../conexao.php");
-session_start();
+@session_start();
 $id_usuario = @$_SESSION['id_usuario'];
 
 $pagina = 'notas_moedas';
@@ -14,52 +14,55 @@ $igreja = isset($_POST['igreja']) ? $_POST['igreja'] : '';
 $id = isset($_POST['id']) ? intval($_POST['id']) : 0;
 $valor = $valor_unitario * $quantidade;
 
+$query_con = $pdo->prepare("SELECT nome FROM tesoureiros WHERE id = :membro AND igreja = :igreja");
+$query_con->bindParam(":membro", $membro);
+$query_con->bindParam(":igreja", $igreja);
+$query_con->execute();
+$res_con = $query_con->fetch(PDO::FETCH_ASSOC);
+
+$nome_membro = ($res_con) ? $res_con['nome'] : 'Membro Não Informado';
+
 if ($id == 0) {
-	$query = $pdo->prepare("INSERT INTO $pagina (tesoureiro, quantidade, valorunitario, valortotal, usuario, igreja, data, tipoinfo) VALUES (:membro, :quantidade, :valor_unitario, :valor, :id_usuario, :igreja, :data, :dizimo_oferta)");
+    $query = $pdo->prepare("INSERT INTO $pagina (tesoureiro, quantidade, valorunitario, valortotal, usuario, igreja, data, tipoinfo) VALUES (:membro, :quantidade, :valor_unitario, :valor, :id_usuario, :igreja, :data, :dizimo_oferta)");
+    $query->bindParam(":membro", $membro);
+    $query->bindParam(":quantidade", $quantidade);
+    $query->bindParam(":valor_unitario", $valor_unitario);
+    $query->bindParam(":valor", $valor);
+    $query->bindParam(":id_usuario", $id_usuario);
+    $query->bindParam(":igreja", $igreja);
+    $query->bindParam(":data", $data);
+    $query->bindParam(":dizimo_oferta", $dizimo_oferta);
+    $query->execute();
+    $ult_id = $pdo->lastInsertId();
 
-	$query->bindParam(':membro', $membro, PDO::PARAM_INT);
-	$query->bindParam(':quantidade', $quantidade, PDO::PARAM_INT);
-	$query->bindParam(':valor_unitario', $valor_unitario, PDO::PARAM_STR);
-	$query->bindParam(':valor', $valor, PDO::PARAM_STR);
-	$query->bindParam(':id_usuario', $id_usuario, PDO::PARAM_INT);
-	$query->bindParam(':igreja', $igreja, PDO::PARAM_STR);
-	$query->bindParam(':data', $data, PDO::PARAM_STR);
-	$query->bindParam(':dizimo_oferta', $dizimo_oferta, PDO::PARAM_STR);
-
-	$query->execute();
-	$ult_id = $pdo->lastInsertId();
-
-	// INSERIR NAS MOVIMENTAÇÕES
-	$movimento_query = $pdo->prepare("INSERT INTO movimentacoes (tipo, movimento, descricao, valor, data, usuario, id_mov, igreja) VALUES ('Entrada', 'Contagem', :nome_membro, :valor, :data, :id_usuario, :ult_id, :igreja)");
-	$movimento_query->bindParam(':nome_membro', $nome_membro, PDO::PARAM_STR);
-	$movimento_query->bindParam(':valor', $valor, PDO::PARAM_STR);
-	$movimento_query->bindParam(':data', $data, PDO::PARAM_STR);
-	$movimento_query->bindParam(':id_usuario', $id_usuario, PDO::PARAM_INT);
-	$movimento_query->bindParam(':ult_id', $ult_id, PDO::PARAM_INT);
-	$movimento_query->bindParam(':igreja', $igreja, PDO::PARAM_STR);
-
-	$movimento_query->execute();
+    // INSERIR NAS MOVIMENTAÇÕES
+    $query_movimentacoes = $pdo->prepare("INSERT INTO movimentacoes (tipo, movimento, descricao, valor, data, usuario, id_mov, igreja) VALUES ('Entrada', 'Contagem', :nome_membro, :valor, :data, :id_usuario, :ult_id, :igreja)");
+    $query_movimentacoes->bindParam(":nome_membro", $nome_membro);
+    $query_movimentacoes->bindParam(":valor", $valor);
+    $query_movimentacoes->bindParam(":data", $data);
+    $query_movimentacoes->bindParam(":id_usuario", $id_usuario);
+    $query_movimentacoes->bindParam(":ult_id", $ult_id);
+    $query_movimentacoes->bindParam(":igreja", $igreja);
+    $query_movimentacoes->execute();
 } else {
-	require_once("../verificar-tesoureiro.php");
-	$query = $pdo->prepare("UPDATE $pagina SET membro = :membro, valor = :valor, data = :data, usuario = :id_usuario, igreja = :igreja WHERE id = :id");
-	$query->bindParam(':membro', $membro, PDO::PARAM_INT);
-	$query->bindParam(':valor', $valor, PDO::PARAM_STR);
-	$query->bindParam(':data', $data, PDO::PARAM_STR);
-	$query->bindParam(':id_usuario', $id_usuario, PDO::PARAM_INT);
-	$query->bindParam(':igreja', $igreja, PDO::PARAM_STR);
-	$query->bindParam(':id', $id, PDO::PARAM_INT);
+    require_once("../verificar-tesoureiro.php");
+    $query = $pdo->prepare("UPDATE $pagina SET tesoureiro = :membro, valor = :valor, data = :data, usuario = :id_usuario, igreja = :igreja WHERE id = :id");
+    $query->bindParam(":membro", $membro);
+    $query->bindParam(":valor", $valor);
+    $query->bindParam(":data", $data);
+    $query->bindParam(":id_usuario", $id_usuario);
+    $query->bindParam(":igreja", $igreja);
+    $query->bindParam(":id", $id);
+    $query->execute();
 
-	$query->execute();
-
-	// ATUALIZAR NAS MOVIMENTAÇÕES
-	$movimento_query = $pdo->prepare("UPDATE movimentacoes SET descricao = :nome_membro, valor = :valor, data = :data, usuario = :id_usuario WHERE id_mov = :id AND movimento = 'Oferta'");
-	$movimento_query->bindParam(':nome_membro', $nome_membro, PDO::PARAM_STR);
-	$movimento_query->bindParam(':valor', $valor, PDO::PARAM_STR);
-	$movimento_query->bindParam(':data', $data, PDO::PARAM_STR);
-	$movimento_query->bindParam(':id_usuario', $id_usuario, PDO::PARAM_INT);
-	$movimento_query->bindParam(':id', $id, PDO::PARAM_INT);
-
-	$movimento_query->execute();
+    // ATUALIZAR NAS MOVIMENTAÇÕES
+    $query_movimentacoes = $pdo->prepare("UPDATE movimentacoes SET descricao = :nome_membro, valor = :valor, data = :data, usuario = :id_usuario WHERE id_mov = :id AND movimento = 'Oferta'");
+    $query_movimentacoes->bindParam(":nome_membro", $nome_membro);
+    $query_movimentacoes->bindParam(":valor", $valor);
+    $query_movimentacoes->bindParam(":data", $data);
+    $query_movimentacoes->bindParam(":id_usuario", $id_usuario);
+    $query_movimentacoes->bindParam(":id", $id);
+    $query_movimentacoes->execute();
 }
 
 echo 'Salvo com Sucesso';
